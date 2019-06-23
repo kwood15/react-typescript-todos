bc2f5010fc6d0d5a10c9dfefe177938ab8f82682
Kay Wood <11405108+kwood15@users.noreply.github.com>
2019-06-23 16:29:25 +0100

    Update component to class

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 type Todos = {
   id: number,
@@ -10,8 +10,16 @@ interface IProps {
   todos: Todos[]
 }
 
-const TodoList = ({ todos }: IProps) => {
-  return todos.map(({ id, title, completed }) => <p key={id}>{title}{completed}</p>
-)}
+// const TodoList = ({ todos }: IProps) => {
+//   return todos.map(({ id, title, completed }: Todos) => <p key={id}>{title}{completed}</p>
+// )}
+
+
+class TodoList extends Component<IProps, {}> {
+  public render() {
+    const { todos } = this.props;
+    return todos.map(({ id, title, completed }: Todos) => <p key={id}>{title}{completed}</p>)
+  }
+}
 
 export default TodoList;
